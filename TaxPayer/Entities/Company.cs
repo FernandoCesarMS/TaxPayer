8dcbac6b359a01ff378521a9bb47d59320d9c7ab
Fernando Cesar <cmnando.silva@gmail.com>
2020-05-24 02:09:07 -0300

    Classes criadas

---
+++ TaxPayer/Entities/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxPayer.Entities
+{
+    class Company : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+        public Company(string name,double anualIncome, double healthExpenditures) : base(name,anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaxPayer/Entities/Individual.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxPayer.Entities
+{
+    class Individual : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Individual(string name,double anualIncome, int numberOfEmployees) : base(name,anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
